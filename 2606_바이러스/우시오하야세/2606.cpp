516d45f8937df02a9cfc80930b1e1f3ecd17ebe3
Ushio-Hayase <ushiohayase@gmail.com>
2025-04-08 21:05:34 +0900

    edit: 2606
    code formatting

--- 2606_바이러스/우시오하야세/2606.cpp
+++ 2606_바이러스/우시오하야세/2606.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int N, M, a, b;
 
-vector<int> v[105];
-int virus[105];
+vector<int> v[101];
+int virus[101];
 int ans;
 
 void dfs(int x)
@@ -32,7 +33,7 @@ int main()
 
     for (int i = 0; i < M; i++)
     {
-        scanf("%d %d", &a, &b);
+        cin >> a >> b;
 
         v[a].push_back(b);
         v[b].push_back(a);
