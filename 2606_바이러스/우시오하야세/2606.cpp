91c5c74e2092631a9c4078f838c5e014d24e1e27
Ushio-Hayase <ushiohayase@gmail.com>
2025-04-08 15:17:09 +0900

    add: post 2606
    from uh

---
+++ 2606_바이러스/우시오하야세/2606.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M, a, b;
+
+vector<int> v[105];
+int virus[105];
+int ans;
+
+void dfs(int x)
+{
+    for (int i = 0; i < v[x].size(); i++)
+    {
+        int k = v[x][i];
+
+        if (virus[k] == 0)
+        {
+            virus[k] = 1;
+            ans++;
+            dfs(k);
+        }
+    }
+
+    return;
+}
+
+int main()
+{
+
+    cin >> N >> M;
+
+    for (int i = 0; i < M; i++)
+    {
+        scanf("%d %d", &a, &b);
+
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+
+    virus[1] = 1;
+    dfs(1);
+
+    cout << ans;
+
+    return 0;
+}
